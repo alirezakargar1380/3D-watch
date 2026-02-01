639a1b4d19917b05ff60a7cfcc3680b0a91ee631
alireza kargar <alirezakargarsuport@gmail.com>
2026-02-01 20:11:34 +0330

    upload project

---
+++ next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  trailingSlash: true,
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
