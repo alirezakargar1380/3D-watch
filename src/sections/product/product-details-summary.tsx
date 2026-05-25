6b48f02bb923074954e34ef18479a8fe04cbf9b3
alireza kargar <alirezakargarsuport@gmail.com>
2026-05-25 09:39:01 +0330

    Update product-details-summary.tsx

--- src/sections/product/product-details-summary.tsx
+++ src/sections/product/product-details-summary.tsx
@@ -49,7 +49,7 @@ export default function ProductDetailsSummary({
   ...other
 }: Props) {
   const router = useRouter();
-  const dialog = useBoolean(true);
+  const dialog = useBoolean(false);
   const {
     id,
     name,
